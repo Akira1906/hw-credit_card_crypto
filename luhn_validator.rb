7a675650b8cf7a8afba784d7b37521116925bfd0
Tristan <tristan.doering@tum.de>
2024-03-09 11:07:02 +0800

    Added validate_checksum() functionality

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -3,9 +3,17 @@ module LuhnValidator
   # arguments: none
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
-  def validate_checksum
-    nums_a = number.to_s.chars.map(&:to_i)
-
-    # TODO: use the integers in nums_a to validate its last check digit
+  def validate_checksum()
+    # number = "17893729974"
+    payload = @number.chars[0..-2].reverse.map.with_index { |c, i| i.even? ? (c.to_i * 2).to_s : c }
+    sum = payload.join.chars.reduce(0) { |s, c| s + c.to_i}
+    check_digit = (10 - (sum % 10)) % 10
+    check_digit == number[-1].to_i
   end
+
 end
+# class CreditCard
+#   include LuhnValidator
+# end
+# card = CreditCard.new
+# puts card.validate_checksum()
